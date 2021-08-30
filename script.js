afd1e747f8c22c3d7a7c8a8f6230f5e77e467093
Supriya Das <das.supriya.supriya@gmail.com>
2021-08-30 11:16:09 +0530

    card front face

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-window.onload = function () {
+window.onload = function() {
+    document.getElementById("cnumber").innerHTML = "####  ####  ####  ####";
+    document.getElementById("holderName").innerHTML = "FULL NAME";
+    document.getElementById("expiry").innerHTML = "MM/YY";
     const monthField = document.getElementById("month-picker");
     const yearField = document.getElementById("year-picker");
-    for(let i=1; i<=12; i++){
-        var option = document.createElement("OPTION");
+    const option = document.createElement("OPTION");
+    const option1 = document.createElement("OPTION");
+    option.innerHTML = "Month";
+    option.value = "";
+    option.disabled = true;
+    option.selected = true;
+    monthField.appendChild(option);
+    option1.innerHTML = "Year";
+    option1.value = "";
+    option1.disabled = true;
+    option1.selected = true;
+    yearField.appendChild(option1);
+    for (let i = 1; i <= 12; i++) {
+        const option = document.createElement("OPTION");
         option.innerHTML = i;
         option.value = i;
         monthField.appendChild(option);
     }
     for (let i = 2010; i <= 2060; i++) {
-        var option = document.createElement("OPTION");
+        const option = document.createElement("OPTION");
         option.innerHTML = i;
         option.value = i;
         yearField.appendChild(option);
